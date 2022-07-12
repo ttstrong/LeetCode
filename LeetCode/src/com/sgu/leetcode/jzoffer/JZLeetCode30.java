14f9bea0943eb04e56386c551cea45d3abd51dc8
caichuqiang <766212085@qq.com>
2022-07-12 10:23:38 +0800

    增加第二种解法（官方）

--- LeetCode/src/com/sgu/leetcode/jzoffer/JZLeetCode30.java
+++ LeetCode/src/com/sgu/leetcode/jzoffer/JZLeetCode30.java
@@ -1,7 +1,10 @@
 package com.sgu.leetcode.jzoffer;
 
+import com.sgu.leetcode.Solution;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * @Author T-T强
@@ -19,36 +22,68 @@ public class JZLeetCode30 {
     //minStack.min();   --> 返回 -2.
 
 
-    Deque<Integer> a;
-    Deque<Integer> b;
+//    Deque<Integer> a;
+//    Deque<Integer> b;
+//
+//    /**
+//     * initialize your data structure here.
+//     */
+//    public JZLeetCode30() {
+//        a = new ArrayDeque<Integer>();
+//        b = new ArrayDeque<Integer>();
+//    }
+//
+//    public void push(int x) {
+//        a.push(x);
+//        if (b.isEmpty() || b.peek() >= x) {
+//            b.push(x);
+//        }
+//    }
+//
+//    public void pop() {
+//        int x = a.pop();
+//        if (x == b.peek()) {
+//            b.pop();
+//        }
+//    }
+//
+//    public int top() {
+//        return a.peek();
+//    }
+//
+//    public int min() {
+//        return b.peek();
+//    }
 
-    /**
-     * initialize your data structure here.
-     */
-    public JZLeetCode30() {
-        a = new ArrayDeque<Integer>();
-        b = new ArrayDeque<Integer>();
+
+    //第二种
+    //思路:
+    //我们可以在每个元素 a 入栈时把当前栈的最小值 m 存储起来。在这之后无论何时，如果栈顶元素是 a，我们就可以直接返回存储的最小值 m。
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
     }
 
     public void push(int x) {
-        a.push(x);
-        if (b.isEmpty() || b.peek() >= x) {
-            b.push(x);
-        }
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
     }
 
     public void pop() {
-        int x = a.pop();
-        if (x == b.peek()) {
-            b.pop();
-        }
+        xStack.pop();
+        minStack.pop();
     }
 
     public int top() {
-        return a.peek();
+        return xStack.peek();
     }
 
     public int min() {
-        return b.peek();
+        return minStack.peek();
     }
 }
