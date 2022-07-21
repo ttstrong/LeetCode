ff369628d62c3c581c696527071a898c28c4fda7
caichuqiang <766212085@qq.com>
2022-07-21 10:40:18 +0800

    最长不含重复字符的子字符串

---
+++ LeetCode/src/com/sgu/leetcode/jzoffer/JZLeetCode48.java
@@ -0,0 +1,31 @@
+package com.sgu.leetcode.jzoffer;
+
+import java.util.*;
+
+/**
+ * @Author T-T强
+ * @Date 2022/7/21 9:48
+ */
+public class JZLeetCode48 {
+    //请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+    //最长不含重复字符的子字符串
+    public int lengthOfLongestSubstring(String s) {
+        int r = 0, l = 0, res = 0;
+        Set<Character> set = new HashSet<>();
+        while (r < s.length()) {
+            char c = s.charAt(r++);
+            while (set.contains(c)) {
+                set.remove(s.charAt(l++));
+            }
+            set.add(c);
+            res = Math.max(res, set.size());
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcac";//3
+        JZLeetCode48 jzLeetCode48 = new JZLeetCode48();
+        System.out.println(jzLeetCode48.lengthOfLongestSubstring(s));
+    }
+}
