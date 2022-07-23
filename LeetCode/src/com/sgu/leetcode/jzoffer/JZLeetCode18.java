f22fc36b1b30caf219d4b91d426e5431a0f0f64a
caichuqiang <766212085@qq.com>
2022-07-23 10:34:52 +0800

    删除链表的节点(修改一点自己的做法)

--- LeetCode/src/com/sgu/leetcode/jzoffer/JZLeetCode18.java
+++ LeetCode/src/com/sgu/leetcode/jzoffer/JZLeetCode18.java
@@ -29,13 +29,24 @@ public class JZLeetCode18 {
 
 
         //单指针
-        if (head == null) return null;
+//        if (head == null) return null;
+//        if (head.val==val) return head.next;
+//        ListNode cur=head;
+//        while (cur.next!=null && cur.next.val!=val)
+//            cur=cur.next;
+//        if (cur.next!=null)
+//            cur.next=cur.next.next;
+//        return head;
+
+
         if (head.val==val) return head.next;
-        ListNode cur=head;
-        while (cur.next!=null && cur.next.val!=val)
+        ListNode pre=head,cur=head.next;
+        while (cur!= null && cur.val!=val){
+            pre=cur;
             cur=cur.next;
-        if (cur.next!=null)
-            cur.next=cur.next.next;
+        }
+        if (cur.val==val)
+            pre.next=cur.next;
         return head;
     }
 }
