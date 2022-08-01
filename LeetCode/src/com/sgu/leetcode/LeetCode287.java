e02a3b2b71e0305961e8de67f9831e42fafa610f
caichuqiang <766212085@qq.com>
2022-08-01 14:26:01 +0800

    寻找重复数(中等)，使用快慢指针 slow = nums[slow]; fast = nums[nums[fast]];

--- LeetCode/src/com/sgu/leetcode/LeetCode287.java
+++ LeetCode/src/com/sgu/leetcode/LeetCode287.java
@@ -13,21 +13,25 @@ public class LeetCode287 {
     //输入：nums = [1,3,4,2,2]
     //输出：2
     public static int findDuplicate(int[] nums) {
-        int slow = 0, fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-        slow = 0;
+        int slow = 0;
+        int fast = 0;
+        slow = nums[slow];
+        fast = nums[nums[fast]];
         while (slow != fast) {
             slow = nums[slow];
-            fast = nums[fast];
+            fast = nums[nums[fast]];
         }
-        return slow;
+        int pre1 = 0;
+        int pre2 = slow;
+        while (pre1 != pre2) {
+            pre1 = nums[pre1];
+            pre2 = nums[pre2];
+        }
+        return pre1;
     }
 
     public static void main(String[] args) {
-        int[] nums={3,1,2,4,2};
+        int[] nums = {3, 1, 2, 4, 2};
         System.out.println(findDuplicate(nums));
     }
 }
