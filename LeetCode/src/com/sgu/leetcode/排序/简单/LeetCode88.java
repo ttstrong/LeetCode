c2eca4d6d958a46dd3ec634e623093b0a15f89e5
caichuqiang <766212085@qq.com>
2022-08-06 11:02:19 +0800

    增加一个从前往后的遍历解法

--- LeetCode/src/com/sgu/leetcode/排序/简单/LeetCode88.java
+++ LeetCode/src/com/sgu/leetcode/排序/简单/LeetCode88.java
@@ -11,21 +11,42 @@ public class LeetCode88 {
     //注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。
     // 为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1, p2 = n - 1;
-        int tail = m + n - 1;
+//        int p1 = m - 1, p2 = n - 1;
+//        int tail = m + n - 1;
+//        int cur;
+//        while (p1 >= 0 || p2 >= 0) {
+//            //p1==-1说明nums1已经没有元素
+//            if (p1 == -1) {
+//                cur = nums2[p2--];
+//            } else if (p2 == -1) {
+//                cur = nums1[p1--];
+//            } else if (nums1[p1] > nums2[p2]) {
+//                cur = nums1[p1--];
+//            } else {
+//                cur = nums2[p2--];
+//            }
+//            nums1[tail--] = cur;
+//        }
+
+
+        int[] sorted = new int[m + n];
+        int p1 = 0, p2 = 0;
         int cur;
-        while (p1 >= 0 || p2 >= 0) {
-            //p1==-1说明nums1已经没有元素
-            if (p1 == -1) {
-                cur = nums2[p2--];
-            } else if (p2 == -1) {
-                cur = nums1[p1--];
-            } else if (nums1[p1] > nums2[p2]) {
-                cur = nums1[p1--];
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+
             } else {
-                cur = nums2[p2--];
+                cur = nums2[p2++];
             }
-            nums1[tail--] = cur;
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i < sorted.length; i++) {
+            nums1[i] = sorted[i];
         }
     }
 }
