b8d321e1b787be894d981b0f709f3706bf6204f6
caichuqiang <766212085@qq.com>
2022-08-28 22:27:26 +0800

    不同路径 II(动态规划)

---
+++ LeetCode/src/com/sgu/leetcode/top100/中等/LeetCode63.java
@@ -0,0 +1,52 @@
+package com.sgu.leetcode.top100.中等;
+
+/**
+ * @Author T-T强
+ * @Date 2022/8/27 15:30
+ */
+public class LeetCode63 {
+    /**
+     * 不同路径 II
+     *
+     * 一个机器人位于一个m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+     * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+     * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+     * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+     * 输出：2
+     * 解释：3x3 网格的正中间有一个障碍物。
+     * 从左上角到右下角一共有 2 条不同的路径：
+     * 1. 向右 -> 向右 -> 向下 -> 向下
+     * 2. 向下 -> 向下 -> 向右 -> 向右
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        //如果开始位置就是障碍物，则直接结束，返回0
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        //如果开始位置不是障碍物，则该格子路径数量为1
+        obstacleGrid[0][0] = 1;
+        //确定第一行格子路径数量
+        for (int i = 1; i < m; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+        //确定第一列格子路径数量
+        for (int i = 1; i < n; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                //如果当前格子不是障碍物，则路过它的有效路径数量为上面格子路径数量 + 左边格子路径数量
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+        return obstacleGrid[m - 1][n - 1];
+    }
+}
