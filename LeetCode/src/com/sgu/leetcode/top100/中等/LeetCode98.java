68e088f7d93f9cef670f3565ca48adcb436c39d1
caichuqiang <766212085@qq.com>
2022-09-01 09:51:59 +0800

    验证二叉搜索树

---
+++ LeetCode/src/com/sgu/leetcode/top100/中等/LeetCode98.java
@@ -0,0 +1,35 @@
+package com.sgu.leetcode.top100.中等;
+
+import com.sgu.leetcode.TreeNode;
+
+/**
+ * @Author T-T强
+ * @Date 2022/9/1 9:24
+ */
+public class LeetCode98 {
+    /**
+     * 验证二叉搜索树
+     * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+     * 有效 二叉搜索树定义如下：
+     * 节点的左子树只包含 小于 当前节点的数。
+     * 节点的右子树只包含 大于 当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     */
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        // 访问右子树
+        return isValidBST(root.right);
+    }
+}
