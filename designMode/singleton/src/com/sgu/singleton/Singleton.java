d0dcf001851c327e81c05a29372f84d7578012d1
caichuqiang <766212085@qq.com>
2022-09-02 08:59:44 +0800

    新增懒汉式：静态内部类

--- designMode/singleton/src/com/sgu/singleton/Singleton.java
+++ designMode/singleton/src/com/sgu/singleton/Singleton.java
@@ -43,6 +43,24 @@ public class Singleton{
 //        return instance;
 //    }
 
+    //懒汉式：静态内部类
+//    public class Singleton {
+//        //私有构造方法
+//        private Singleton() {}
+//
+//        //定义一个静态内部类
+//        private static class SingletonHolder{
+//            //在内部类中声明并初始化外部类的对象
+//            private static final Singleton INSTANCE  = new Singleton();
+//        }
+//
+//        //提供公共访问方式
+//        public static Singleton getInstance(){
+//            return SingletonHolder.INSTANCE;
+//        }
+//    }
+
+
     /**
      * 懒汉式-双重校验锁-线程安全
      */
