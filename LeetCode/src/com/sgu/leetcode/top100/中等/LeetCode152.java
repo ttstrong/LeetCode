c49bf03784e01024a2bb53691811f433d988694a
caichuqiang <766212085@qq.com>
2022-09-15 14:52:52 +0800

    乘积最大子数组

---
+++ LeetCode/src/com/sgu/leetcode/top100/中等/LeetCode152.java
@@ -0,0 +1,40 @@
+package com.sgu.leetcode.top100.中等;
+
+/**
+ * @Author T-T强
+ * @Date 2022/9/15 8:50
+ */
+public class LeetCode152 {
+    //乘积最大子数组
+
+    /**
+     * 给你一个整数数组 nums，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+     * 测试用例的答案是一个32-位 整数。
+     * 子数组 是数组的连续子序列。
+     * 输入: nums = [2,3,-2,4]
+     * 输出: 6
+     * 解释: 子数组 [2,3] 有最大乘积 6
+     */
+    public static int maxProduct(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int dpMax = nums[0];
+        int dpMin = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            //更新 dpMin 的时候需要 dpMax 之前的信息，所以先保存起来
+            int preMax = dpMax;
+            dpMax = Math.max(dpMin * nums[i], Math.max(dpMax * nums[i], nums[i]));
+            dpMin = Math.min(dpMin * nums[i], Math.min(preMax * nums[i], nums[i]));
+            max = Math.max(max, dpMax);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={-2,3,-4};
+        System.out.println(maxProduct(nums));
+    }
+}
